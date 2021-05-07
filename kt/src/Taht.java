43949a382d3cb83dcadf317ec7dfa481f48e5ea2
Karlp <karlp@tlu.ee>
2021-05-07 23:24:00 +0300

    commiting

--- kt/src/Taht.java
+++ kt/src/Taht.java
@@ -10,17 +10,17 @@ interface Taht {
     Integer sonadeArv();
 }
 
-
-
 class LetterReader implements Taht {
 
 
     public static void main(String[] args)  {
+
+        //READING SYSTEM IN
         System.out.println("Sisestage sona");
         Scanner scan = new Scanner(System.in);
         String answer = scan.nextLine();
         System.out.println(answer);
-
+        //WRITING TO FILE WITHOUT OVERWRITING
         File log = new File("MyFile.txt");
         try{
             if(log.exists()==false){
@@ -34,6 +34,8 @@ class LetterReader implements Taht {
             System.out.println("COULD NOT LOG!!");
         }
 
+
+        //COUNTING A-s IN WORDS
         int totalCharacters = 0;
         int totalCharactersInSentence = 0;
         int counter = 0;
@@ -46,6 +48,7 @@ class LetterReader implements Taht {
         String[] newAnswer = new String[0];
         for (int i = 0; i < answer.length(); i++) {
             newAnswer = answer.split(" ");
+            //COUNTING A-s IN SENTENCES
         }
         for (String i : newAnswer) {
             totalCharactersInSentence = 0;
@@ -55,6 +58,50 @@ class LetterReader implements Taht {
                 if (temp == 'a')
                     totalCharactersInSentence++;
             }
+            //DELETING LINES THAT ARE THE SAME
+            try {
+                PrintWriter pw = new PrintWriter("Output.txt");
+
+                // BufferedReader object for input.txt
+                BufferedReader br1 = new BufferedReader(new FileReader("MyFile.txt"));
+// TODO: 07.05.2021 TEHA JUURDE DELETE.TXT (FAILI SISUKS ON IGAKORD ÜLEKIRJUTATAV TEKST SYSTEM.IN POOLT) 
+                String line1 = br1.readLine();
+
+                while(line1 != null)
+                {
+                    boolean flag = false;
+
+                    // BufferedReader object for delete.txt
+                    BufferedReader br2 = new BufferedReader(new FileReader("delete.txt"));
+
+                    String line2 = br2.readLine();
+
+                    // loop for each line of delete.txt
+                    while(line2 != null)
+                    {
+                        if(line1.equals(line2))
+                        {
+                            flag = true;
+                            break;
+                        }
+
+                        line2 = br2.readLine();
+                    }
+
+                    // if flag = false
+                    // write line of input.txt to output.txt
+                    if(!flag)
+                        pw.println(line1);
+
+                    line1 = br1.readLine();
+
+                }
+            }
+            catch (Exception e){
+                System.out.println("Exeption");
+            }
+
+
 
         System.out.println("Terve lause a tähtede arv on: "+totalCharacters);
         System.out.println(counter+ ". lauses on " +" " + totalCharactersInSentence + " a tähte");
