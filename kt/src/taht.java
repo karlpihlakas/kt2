4233a81c5bac640ca09a08fecc5a3f564fc7b61b
Karlp <karlp@tlu.ee>
2021-05-07 13:08:44 +0300

    initial commit

---
+++ kt/src/taht.java
@@ -0,0 +1,13 @@
+public interface taht {
+    public void letter();
+}
+
+class LetterReader implements taht{
+
+    @Override
+    public void letter() {
+        for (int i = 0; i < 10; i++) {
+
+        }
+    }
+}
